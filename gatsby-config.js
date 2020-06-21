adaa88868a72eb9efdaf0326b29c72aaca4bbe8f
audeta35 <audeta35@gmail.com>
2020-06-22 04:16:43 +0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,8 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Deta Portfolio`,
+    siteTitleAlt: `Audeta - Portfolio`,
   },
   plugins: [
     {
@@ -16,17 +17,11 @@ module.exports = {
       options: {},
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Audeta Sandy Bima Satria`,
+        short_name: `Deta`,
+        description: `Portfolio Saya`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
